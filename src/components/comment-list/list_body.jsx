1576bdff2200803b03ae38682a5354a50ad28d6f
陈少桐 <1528841402@qq.com>
2020-06-22 14:38:26 +0800

    update

---
+++ src/components/comment-list/list_body.jsx
@@ -0,0 +1,16 @@
+// @flow strict
+
+import * as React from 'react';
+
+
+class list_body extends React.Component{
+    render() {
+        return (
+            <div>
+                body...
+            </div>
+        );
+    }
+}
+
+export default list_body;
