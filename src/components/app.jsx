6c8b87505eed9b6648987e7d98c7857869d67b97
陈少桐 <1528841402@qq.com>
2020-06-22 12:04:41 +0800

    1

---
+++ src/components/app.jsx
@@ -0,0 +1,17 @@
+// @flow strict
+
+import * as React from 'react';
+
+type Props = {||};
+
+class app extends React.Component<Props> {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default app;
