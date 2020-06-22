e820979c1b1b81a780b74921bd8a50bed87d9121
陈少桐 <1528841402@qq.com>
2020-06-22 16:13:02 +0800

    submit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import ComponentList from './components/comment-list/comment-list'
 
 function App() {
+
   return (
     <div className="App">
+      <ComponentList />
     </div>
   );
 }
